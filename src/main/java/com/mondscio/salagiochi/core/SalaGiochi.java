84e3faf9c68ec327cb6ca34ab177b9ae321e8d88
Andrea <annis.mond@gmail.com>
2025-10-11 12:55:06 +0200

    .

--- src/main/java/com/mondscio/salagiochi/core/SalaGiochi.java
+++ src/main/java/com/mondscio/salagiochi/core/SalaGiochi.java
@@ -2,6 +2,6 @@ package com.mondscio.salagiochi.core;
 
 public class SalaGiochi {
 	public void stampa() {
-		System.out.print("prova di commit & push.");
+		System.out.print("prova di commit & push. (!)");
 	}
 }
