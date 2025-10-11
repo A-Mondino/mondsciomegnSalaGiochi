4b8f3d51a4697edc622d2cd19f1ddcd45cf31c2c
Andrea <annis.mond@gmail.com>
2025-10-11 12:33:45 +0200

    prova di push e commit dopo il collegamento alla nostra repo

--- src/main/java/com/mondscio/salagiochi/core/Main.java
+++ src/main/java/com/mondscio/salagiochi/core/Main.java
@@ -3,7 +3,7 @@ package com.mondscio.salagiochi.core;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("prova di commit & push");
+		System.out.print("prova di commit & push.");
 
 	}
 
